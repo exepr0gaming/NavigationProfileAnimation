cdac257162bf24102106f9089755e062034b47a8
exepr0gaming <exehomework@gmail.com>
2023-07-21 12:36:47 +0300

    Initial Commit

---
+++ NavigationProfileAnimation/NavigationProfileAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationProfileAnimationApp.swift
+//  NavigationProfileAnimation
+//
+//  Created by Andrew Kurdin on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationProfileAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
